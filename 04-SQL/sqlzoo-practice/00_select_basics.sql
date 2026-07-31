e7942cab994b285bb82078eca984593eee70282c
rajaehtasham <rajaehtasham1921@gmail.com>
2026-07-31 13:35:15 +0500

    solve SELECT Basics exercise 1 (filter by exact country)

---
+++ 04-SQL/sqlzoo-practice/00_select_basics.sql
@@ -0,0 +1,7 @@
+-- Topic: SELECT Basics (Exercise 1)
+-- Platform: SQLZoo (Section 0)
+-- Retrieves the population of Germany from the world table
+
+SELECT population 
+FROM world 
+WHERE name = 'Germany'
