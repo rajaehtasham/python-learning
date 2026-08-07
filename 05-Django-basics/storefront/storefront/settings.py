9c88cab993e024158d240c81570a9099b5bda4bf
rajaehtasham <rajaehtasham1921@gmail.com>
2026-08-07 15:20:09 +0500

    Add INTERNAL_IPS configuration for Django Debug Toolbar

--- 05-Django-basics/storefront/storefront/settings.py
+++ 05-Django-basics/storefront/storefront/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'playground'
     'debug_toolbar'
 ]
-
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
